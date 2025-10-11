36b23ef8ef7b50d8c435be4f36cfaa2f0c1a1191
source-ful <monchromatic414@gmail.com>
2025-10-11 12:16:56 -0500

    Did some work today, primarily learning about libraries and their function.

---
+++ draft.py
@@ -0,0 +1,21 @@
+"""
+
+This script functions as a precursour for the farming.py script, due to my lack of knowledge of the libraries used.
+As a result, this simple script will function to teach my the libraries aswell as principles of automation.
+
+This script will not be used in the final product, but will be used to teach me the libraries and principles of automation.
+Note: While this script will not be used in the final product, it will be included in the repository for reference & documentation.
+
+"""
+# Importing libraries
+
+import keyboard
+import mouse
+import time
+import threading    
+from pynput.mouse import Controller, Button
+
+
+# ============================================
+
+
